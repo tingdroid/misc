0de14d9ba613e820af760dcdfb717240b74aca1c
Ting Tron <olegyk@tingtronics.com>
2013-03-02 16:16:05 -0500

    added XYZ axis

--- TingJpct2/src/com/ting/app/MyActivity.java
+++ TingJpct2/src/com/ting/app/MyActivity.java
@@ -7,6 +7,7 @@ import com.threed.jpct.Primitives;
 import com.threed.jpct.SimpleVector;
 import com.threed.jpct.World;
 import android.widget.SimpleCursorAdapter;
+import com.threed.jpct.RGBColor;
 
 /**
  * A simple demo. 
@@ -15,8 +16,15 @@ import android.widget.SimpleCursorAdapter;
 public class MyActivity extends GLActivity {
 
 	private Object3D cube = null;
+	private Object3D center = null;
 	SimpleVector sunOffset = new SimpleVector(-100, -100, -75);
 
+	void add(Object3D obj) {
+		obj.strip();
+		obj.build();
+		world.addObject(obj);		
+	}
+	
 	@Override
     void init() {		
 		world = new World();
@@ -25,9 +33,29 @@ public class MyActivity extends GLActivity {
 		cube = Primitives.getCube(10);
 		cube.calcTextureWrapSpherical();
 		cube.setTexture(addTexture("texture", R.drawable.icon, 64, 64));
-		cube.strip();
-		cube.build();
-		world.addObject(cube);
+		add(cube);
+
+		center = Primitives.getSphere(1);
+		add(center);
+		center.addChild(cube);
+
+		Object3D coneX = Primitives.getCone(1);
+		coneX.translate(15, 0, 0);
+		coneX.setAdditionalColor(new RGBColor(255, 0, 0));
+		center.addChild(coneX);
+		add(coneX);
+		
+		Object3D coneY = Primitives.getCone(1);
+		coneY.translate(0, 15, 0);
+		coneY.setAdditionalColor(new RGBColor(0, 255, 0));
+		center.addChild(coneY);
+		add(coneY);
+
+		Object3D coneZ = Primitives.getCone(1);
+		coneZ.translate(0, 0, 15);
+		coneZ.setAdditionalColor(new RGBColor(0, 0, 255));
+		center.addChild(coneZ);
+		add(coneZ);
 
 		Camera cam = world.getCamera();
 		cam.moveCamera(Camera.CAMERA_MOVEOUT, 50);
@@ -41,11 +69,11 @@ public class MyActivity extends GLActivity {
 	@Override
 	void move(float dx, float dy) {
 		if (dx != 0) {
-			cube.rotateY(dx/-100f);
+			center.rotateY(dx/-100f);
 		}
 
 		if (dy != 0) {
-			cube.rotateX(dy/-100f);
+			center.rotateX(dy/-100f);
 		}
 	}
 }
